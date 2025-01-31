e5c60daf43e77c23abdc5ff9829e33672d2b4c21
Ринат <ignat934@gmail.com>
2025-01-31 21:06:33 +0300

    поправлено ридми

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,19 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.CommentService;
-import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
 @Controller
-//@RequestMapping("/")
 @RequiredArgsConstructor
 public class MainController {
 
